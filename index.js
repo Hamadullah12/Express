d11b5e9a9bda24a2d2f44033e3f2179de7658b74
Hamadullah12 <hamadfms05@gmail.com>
2025-03-13 20:19:03 +0800

    having fun with bugs in sending path parameter

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+
+// let port = 8080;
+// app.listen(port, () => {
+//   console.log(`Expreess app is listening ${port}`);
+//   app.get("/", (req, res) => {
+//     res.send("successfully connected to the get method");
+//   });
+//   app.get("/:search/:username", (req, res) => {
+//     let { search, username } = req.params;
+//     res.send(`successfully connected to the search path of @${search} and with username fo ${username}`);
+//   });
+//   app.get("/contact", (req, res) => {
+//     res.send("successfully connected to the contaced path");
+//   });
+//   app.get("*", (req, res) => {
+//     res.send("this does not contain");
+//   });
+//   app.post("/root", (req, res) => {
+//     res.send("You requed to the post root defination  method");
+//   });
+// });
+
+// // app.use((req, res) => {
+// //   // console.log(res);
+// //   console.log("request received successfully");
+// //   // res.send("Response has been sent successfully");
+// //   res.send({
+// //     name: "Hamad Ullah",
+// //     startup: "To start a food buissnes ",
+// //     inspiration: "My Mother",
+// //     Yeartoacheive: 2027,
+// //   });
+// // });
+
+// path pararmeterrs
+const express = require("express"); //we have to import express
 const app = express();
 
-let port = 8080;
+let port = 8080; //locall server
 app.listen(port, () => {
-  console.log(`Expreess app is listening ${port}`);
-});
-
-app.use((req, res) => {
-  // console.log(res);
-  console.log("request received successfully");
-  // res.send("Response has been sent successfully");
-  res.send({
-    name: "Hamad Ullah",
-    startup: "To start a food buissnes ",
-    inspiration: "My Mother",
-    Yeartoacheive: 2027,
+  //it listen the request
+  console.log(`${port} server is succesfully running`);
+  app.get("/:username/:id", (req, res) => {
+    //these are paht parameters with get method
+    let { username, id } = req.params; //params are parameters that is used to display the paramerter that we pass
+    let htmlCode = `<h1>The user @${username} has <p> been enrolled </p> with id: ${id} </h1>`;
+    res.send(htmlCode); //at the end we send the response
   });
 });
